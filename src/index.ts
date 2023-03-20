11d516ab68450c1a1d8b3b8f8674fdbf412e7eff
Max Kramer <max@kramer.so>
2023-03-20 21:12:38 +0100

    chore: filters out duplicates

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import * as playwright from "playwright"
 import * as dotenv from 'dotenv'
 import {postProperties} from "./slack"
 import {closeDatabase, db, loadDatabase} from "./helpers/database"
-import Cron from "croner"
+import { Cron } from "croner"
 
 const fetchAllProperties = async () => {
     const results = []
@@ -30,15 +30,24 @@ const main = () => {
     loadDatabase
         .then(fetchAllProperties)
         .then((adapters) => adapters.map((adapter) => {
-            const collection = db.getCollection(adapter.config.name) || db.addCollection(adapter.config.name)
-            const existingApartments = collection.find({
+            const collection = db.getCollection(adapter.config.name) ?? db.addCollection(adapter.config.name)
+            const existingApartments = new Set(collection.find({
                 id: {
                     "$in": adapter.properties.map((apartment) => apartment.id)
                 },
-            }).map((apartment) => apartment.id)
+            }).map((apartment) => apartment.id))
 
-            const newApartments = adapter.properties.filter((apartment) => existingApartments.find((ap) => ap === apartment.id) === undefined)
-            console.log(`Found ${newApartments.length} new apartments on ${adapter.config.name}`)
+            const duplicatesHashMap: Record<string, object> = {}
+            const newApartments = adapter.properties.filter((apartment) => {
+                if (duplicatesHashMap[apartment.id] === undefined) {
+                    duplicatesHashMap[apartment.id] = {}
+                    return !existingApartments.has(apartment.id)
+                }
+                return false
+            })
+
+            console.log(`Found ${newApartments.length}/${adapter.properties.length} new apartments on ${adapter.config.name}`)
+            console.log(newApartments.map((ap) => ap.url))
 
             collection.startTransaction()
             collection.insert(newApartments)
@@ -47,13 +56,16 @@ const main = () => {
 
             return {newProperties: newApartments, adapter: adapter.config}
         }))
-        .then(postProperties)
+        // .then(postProperties)
         .then(() => closeDatabase)
         .then(() => console.log("Completed job"))
 }
 
 Cron(
-    '0 */5 5-22 * * *',
-    {catch: true},
-    main
+    '0 */5 5-23 * * *',
+    {
+        catch: true,
+        unref: false,
+    },
+    main,
 )
